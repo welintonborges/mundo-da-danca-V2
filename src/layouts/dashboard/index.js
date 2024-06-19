8d0cbb1a17ac757756613b7cdd830c76534ca30e
welintonborges <welinton.b.borges@gmail>
2024-06-19 16:27:47 -0300

    Adicionando menu dash

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -40,7 +40,7 @@ function Dashboard() {
 
   return (
     <DashboardLayout>
-      {/*<DashboardNavbar />*/}
+      <DashboardNavbar />
       <MDBox py={3}>
         <Grid container spacing={3}>
           <Grid item xs={12} md={6} lg={3}>
